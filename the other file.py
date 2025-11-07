74dc3fefac2abb5ebee23b61daceb40234dc2068
bmartin435 <bmartin435@phm.k12.in.us>
2025-11-07 09:48:37 -0500

    Update the other file.py

--- the other file.py
+++ the other file.py
@@ -5,21 +5,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-sns.countplot(x=y_train)
+
+#mnist = tf.keras.datasets.mnist
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#sns.countplot(x=y_train)
+from keras.datasets import cifar10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 plt.show()
 
 print("Any NaN Training: ", np.isnan(x_train).any())
 print("Any NaN Training: ", np.isnan(x_test).any())
 
-input_shape = (28,28,1)
+input_shape = (32,32,3)
 
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
 x_train = x_train /255.0
 
 x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
 x_test = x_test /255.0
+
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
 
 #convert tables to one hot
 y_train = tf.one_hot(y_train.astype(np.int32), depth = 10)
@@ -39,18 +45,27 @@ model = tf.keras.models.Sequential(
     [
         tf.keras.layers.Conv2D(64, (5,5), padding="same", activation="relu", input_shape=input_shape),
         tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="relu", input_shape=input_shape),
-   #     tf.keras.layers.MaxPool2D(),
-   #     tf.keras.layers.Dropout(0.25),
-   #     tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="relu", input_shape=input_shape),
-   #     tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="relu", input_shape=input_shape),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="relu", input_shape=input_shape),
+        tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="relu", input_shape=input_shape),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(num_classes, activation="softmax"),
     ]
 )
 
-model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss="categorical_crossentropy", metrics=["acc"])
+from keras.utils import to_categorical
 
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+
+#model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss="categorical_crossentropy", metrics=["acc"])
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['acc'])
+
+#history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+history = model.fit(x_train, y_train,epochs=10,validation_data=(x_test, y_test))
+
 
 #plot out training and validation accuracy and loss
 fig, ax = plt.subplots(2,1)
